1eec1942bd78e4af1c9878551d78dcae04e2eaa3
Kaique <kakaluzrda@gmail.com>
2022-06-22 14:32:14 -0300

    main

---
+++ simulator/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	log
+)
+
+func init() {
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(Error loading .env file)
+	}
+}
+
+func main() {
+	producer = kafka.NewKafkaProducer()
+	kafka.Publish(msgOla, topicreadteste, producer)
+
+	route = route2.Route{
+		ID       1,
+		ClientID 1,
+	}
+
+	route.LoadPositions()
+	stringsjon, _ = route.ExportJsonPositions()
+	fmt.Println(stringsjon[0])
+}
